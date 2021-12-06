e2d8476e739bc275e44c6ed2989f673b5ae855ee
coderlore <coderlore@gmail.com>
2021-12-06 08:32:39 -1000

    Added current weather conditions to webpage

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import "../dist/style.css";
 
-const content = document.querySelector("#content");
+const content = document.querySelector("#current-condition");
 const currentCity = document.createElement("div");
 content.appendChild(currentCity);
+
+const currentTemp = document.createElement("div");
+content.appendChild(currentTemp);
+
+const currentWeather = document.createElement("div");
+content.appendChild(currentWeather);
 
 async function getWeather() {
   const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=honolulu&appid=e563f19376073b9fb01a1ef1111b8442&units=imperial", { mode: "cors" });
   const weatherData = await response.json();
-  const currentCondition = weatherData.weather.main;
   console.log(weatherData);
-  console.log(currentCondition);
   currentCity.innerHTML = weatherData.name;
+  currentTemp.innerHTML = weatherData.main.temp;
+  currentWeather.innerHTML = weatherData.weather[0].main;
 }
 
 getWeather();
